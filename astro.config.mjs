577edfd5a30dece3e4e9dd390a497137a3009882
yashpatel20 <yashpatel20>
2024-07-07 23:12:51 -0400

    update theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
 });
